ddaeb04b52b6880fdbd2351050d5f968aa0b4411
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-09 07:25:51 +0000

    feat: initial experience and blog pages

---
+++ apps/frontend/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default async function BlogPage() {
+  return (
+    <div>
+      <p>blog-page works!</p>
+    </div>
+  );
+}
---
+++ apps/frontend/app/experience/page.tsx
@@ -0,0 +1,7 @@
+export default async function ExperiencePage() {
+  return (
+    <div>
+      <p>experience-page works!</p>
+    </div>
+  );
+}
