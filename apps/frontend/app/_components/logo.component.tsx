c120109cb97975fbb6ff8ae0f873cce6c422e359
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-10 04:43:09 +0000

    refactor: fill the container of the image instead of fix height and width from db

--- apps/frontend/app/_components/logo.component.tsx
+++ apps/frontend/app/_components/logo.component.tsx
@@ -1,20 +1,26 @@
 import Image from 'next/image';
 import { Logo } from '../_gql/landing-page.interface';
 
-type LogoComponentProps = Logo;
+export interface LogoComponentProps extends Logo {
+  className?: string;
+}
 
 export const LogoComponent = (props: LogoComponentProps) => {
   const {
-    image: { url, width, height },
+    image: { url },
     altText,
+    className = '',
   } = props;
 
   return (
     <Image
+      className={className}
       src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${url}`}
       alt={altText}
-      width={width}
-      height={height}
+      unoptimized
+      fill
+      priority
+      objectFit="contain"
     />
   );
 };
