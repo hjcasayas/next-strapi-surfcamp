423cacb7d080441d1031a4e41e375c7eb8dc6e97
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-11 14:45:23 +0000

    feat: initial newsletter subscription functionality

--- apps/frontend/app/_components/newsletter-form.component.tsx
+++ apps/frontend/app/_components/newsletter-form.component.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Markdown from 'react-markdown';
 import { NewsletterForm } from '../_interfaces/newsletter-form.interface';
+import { useState } from 'react';
 
 export const NewsletterFormComponent = ({
   title,
@@ -7,6 +10,14 @@ export const NewsletterFormComponent = ({
   inputPlaceholder,
   buttonLabel,
 }: NewsletterForm) => {
+  const [email, setEmail] = useState<string>('');
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Email submitted:', email);
+    setEmail('');
+  };
+
   return (
     <div className="bg-light-brown flex items-center justify-between rounded-4xl py-10 pr-5 pl-12">
       <div className="flex w-[55%] flex-col space-y-6">
@@ -15,13 +26,18 @@ export const NewsletterFormComponent = ({
           <Markdown>{description}</Markdown>
         </div>
       </div>
-      <form className="nowrap flex w-[45%] flex-row overflow-hidden rounded-[18px]">
+      <form
+        onSubmit={onSubmit}
+        className="nowrap flex w-[45%] flex-row overflow-hidden rounded-[18px]"
+      >
         <input
           id="email"
           name="email"
           type="email"
           placeholder={inputPlaceholder}
           className="inline-block flex-1 bg-[#FFFBF4] py-5 pr-3 pl-5 text-xl outline-0"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <button className="bg-turquoise inline-block cursor-pointer px-12 py-5 text-xl font-bold text-white">
           {buttonLabel}
