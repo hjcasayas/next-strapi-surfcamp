bc9fbc672f9ba0039f054f449ba2560e3b9f42ee
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-12 03:29:10 +0000

    feat: format date

--- apps/frontend/app/_components/featured-item/featured-item-date.component.tsx
+++ apps/frontend/app/_components/featured-item/featured-item-date.component.tsx
@@ -1,7 +1,13 @@
+import { format } from 'date-fns';
+
 interface FeaturedItemDateProps {
   date: string;
 }
 
 export const FeaturedItemDateComponent = ({ date }: FeaturedItemDateProps) => {
-  return <span className="inline-block text-[15px]">{date}</span>;
+  return (
+    <span className="inline-block text-[15px]">
+      {format(new Date(date), 'EEEE, LLLL dd, yyyy')}
+    </span>
+  );
 };
