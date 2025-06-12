efe68e56637af4584bcff42da01cce8b53086d27
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-12 16:15:04 +0000

    feat: added see more button functionality

--- apps/frontend/app/_components/featured-articles.component.tsx
+++ apps/frontend/app/_components/featured-articles.component.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { FeaturedArticles } from '../_interfaces/featured-articles.interface';
 import { cn } from '../_utils/cn.util';
 
@@ -7,6 +10,18 @@ export const FeaturedArticlesComponent = ({
   articles,
   button: { label, bgColorClassName, textColorClassName },
 }: FeaturedArticles) => {
+  const [numberOfArticles, setNumberOfArticles] = useState(3);
+
+  const handleClick = () => {
+    setNumberOfArticles((prev) => {
+      if (prev + 3 > articles.length) {
+        return articles.length;
+      }
+
+      return prev + 3;
+    });
+  };
+
   const textColorClassNameMap: Record<string, string> = {
     text_white: 'text-white',
     text_black: 'text-black',
@@ -23,20 +38,23 @@ export const FeaturedArticlesComponent = ({
     <div className="flex flex-col gap-y-[60px]">
       <h2 className="text-6xl">Featured Articles</h2>
       <div className="grid grid-cols-3 gap-x-[3%] gap-y-[4%]">
-        {articles.map((article) => (
+        {articles.slice(0, numberOfArticles).map((article) => (
           <FeaturedArticleComponent key={article.slug} {...article} />
         ))}
       </div>
-      <button
-        type="button"
-        className={cn(
-          'inline-block self-center rounded-full px-12 py-4 text-3xl font-bold',
-          bgColorClassNameMap[bgColorClassName],
-          textColorClassNameMap[textColorClassName]
-        )}
-      >
-        {label}
-      </button>
+      {numberOfArticles < articles.length ? (
+        <button
+          onClick={handleClick}
+          type="button"
+          className={cn(
+            'inline-block self-center rounded-full px-12 py-4 text-3xl font-bold',
+            bgColorClassNameMap[bgColorClassName],
+            textColorClassNameMap[textColorClassName]
+          )}
+        >
+          {label}
+        </button>
+      ) : null}
     </div>
   );
 };
