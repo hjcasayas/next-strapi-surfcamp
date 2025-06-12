085bcf79da762b2bce876f240f9717ff7ddc527b
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-12 16:13:45 +0000

    feat: prefetch data for blog page and experience page

--- apps/frontend/app/blog/page.tsx
+++ apps/frontend/app/blog/page.tsx
@@ -1,5 +1,15 @@
 import { BlogPageComponent } from '../_components/pages/blog-page.component';
+import { getQueryClient } from '../_utils/react-query.util';
+import { graphqlRequest } from '../_gql/graphql-request';
+import { blogPageQuery } from '../_gql/blog-page.query';
 
 export default async function BlogPage() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['blogPage'],
+    queryFn: () => graphqlRequest(blogPageQuery),
+  });
+
   return <BlogPageComponent />;
 }
--- apps/frontend/app/experience/page.tsx
+++ apps/frontend/app/experience/page.tsx
@@ -1,5 +1,15 @@
 import { ExperiencePageComponent } from '../_components/pages/experience-page.component';
+import { experiencePageQuery } from '../_gql/experience-page.query';
+import { graphqlRequest } from '../_gql/graphql-request';
+import { getQueryClient } from '../_utils/react-query.util';
 
 export default async function ExperiencePage() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['experiencePage'],
+    queryFn: () => graphqlRequest(experiencePageQuery),
+  });
+
   return <ExperiencePageComponent />;
 }
