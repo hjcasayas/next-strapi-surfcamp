c965672cfdc1d5d644b6bed06eb220d2bb1a5544
hjcasayas <henlyjade.casayas@gmail.com>
2025-06-13 07:11:38 +0000

    feat: added variables as parameters of useQueryPage hook

--- apps/frontend/app/_hooks/use-landing-page-query.hook.ts
+++ apps/frontend/app/_hooks/use-landing-page-query.hook.ts
@@ -1,15 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { graphqlRequest } from '../_gql/graphql-request';
 
-export const usePageQuery = <TData>({
+export const usePageQuery = <TData, TVariables = never>({
   queryKeys,
   gqlQuery,
+  variables,
 }: {
   queryKeys: string[];
   gqlQuery: string;
+  variables?: TVariables;
 }) => {
   return useQuery<TData>({
     queryKey: queryKeys,
-    queryFn: () => graphqlRequest<TData>(gqlQuery),
+    queryFn: () => graphqlRequest<TData, TVariables>(gqlQuery, variables),
   });
 };
